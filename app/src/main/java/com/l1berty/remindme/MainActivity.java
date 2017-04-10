32697077621e1326282e63ae85f125c43a406ab3
Anton Konobeev <konobeev.anton@gmail.com>
2017-04-10 16:05:47 +0300

    Created MainActivity class and layout

---
+++ app/src/main/java/com/l1berty/remindme/MainActivity.java
@@ -0,0 +1,14 @@
+package com.l1berty.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
